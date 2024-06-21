2da87e9c69c82e928e14c4573fb6588c2c08d712
Thisisgerry123 <167911356+Thisisgerry123@users.noreply.github.com>
2024-06-21 02:45:48 +0000

    started project

---
+++ app.js
@@ -0,0 +1,22 @@
+// This is my function that sums two numbers
+//const sum = (a,b) => {
+    //return a + b
+}
+
+// Just a console log for ourselves
+//console.log(sum(7,3))
+
+// Export the function to be used on other files 
+// (similar to the keyword "export" when using webpack)
+//var exports = { sum };
+
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+    }
+let oneYenIs = {
+    "USD": 156.5,
+    "GBP": 0.0050,
+    "EURO": 170.26
+}
